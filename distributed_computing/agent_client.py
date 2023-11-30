07a0257f4f9dfed0923118d81a66b80f050ba10b
tusjogren <sjoegren@campus.tu-berlin.de>
2023-11-30 12:57:43 +0100

    added code for assignment distributed computing

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -7,6 +7,11 @@
 '''
 
 import weakref
+import threading
+import xmlrpc.client
+import sys
+import os
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
@@ -16,12 +21,15 @@ class PostHandler(object):
 
     def execute_keyframes(self, keyframes):
         '''non-blocking call of ClientAgent.execute_keyframes'''
-        # YOUR CODE HERE
+        thread = threading.Thread(target=self.proxy.execute_keyframes, args=[keyframes])
+        thread.start()
+        thread.join()
 
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
-        # YOUR CODE HERE
-
+        thread = threading.Thread(target=self.proxy.set_transform, args=[effector_name, transform])
+        thread.start()
+        thread.join()
 
 class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
@@ -29,38 +37,41 @@ class ClientAgent(object):
     # YOUR CODE HERE
     def __init__(self):
         self.post = PostHandler(self)
+        self.proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
-        # YOUR CODE HERE
+        angle = self.proxy.get_angle(joint_name)
+        return angle
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
-        # YOUR CODE HERE
+        self.proxy.set_angle(joint_name, angle)
 
     def get_posture(self):
         '''return current posture of robot'''
-        # YOUR CODE HERE
+        posture = self.proxy.get_posture()
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
-        # YOUR CODE HERE
+        self.proxy.execute_keyframes(keyframes)
 
     def get_transform(self, name):
         '''get transform with given name
         '''
-        # YOUR CODE HERE
+        transform = self.proxy.get_transform(name)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
-        # YOUR CODE HERE
+        self.proxy.set_transform(effector_name, transform)
 
 if __name__ == '__main__':
     agent = ClientAgent()
-    # TEST CODE HERE
-
-
+    print("Angle before change: " + str(agent.get_angle("LShoulderPitch")))
+    agent.set_angle("LShoulderPitch", 0.8)
+    print("Angle after change: " + str(agent.get_angle("LShoulderPitch")))
+    print(agent.get_posture())
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -14,7 +14,10 @@
 # add PYTHONPATH
 import os
 import sys
+import time
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
+from xmlrpc.server import SimpleXMLRPCServer
+import threading
 
 from inverse_kinematics import InverseKinematicsAgent
 
@@ -23,35 +26,83 @@ class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
     # YOUR CODE HERE
-    
+    def __init__(self):
+        # Call the constructor of the parent class 'ServerAgent'
+        super(ServerAgent, self).__init__()
+
+        # Create a simple XML-RPC server bound to localhost on port 8000
+        server = SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
+
+        # Register an instance of the current class (ServerAgent) to handle requests
+        server.register_instance(self)
+
+        # Register several functions to be accessible via XML-RPC
+        # These functions allow remote control over certain functionalities
+        server.register_function(self.set_angle, "set_angle")
+        server.register_function(self.get_angle, "get_angle")
+        server.register_function(self.get_posture, "get_posture")
+        server.register_function(self.execute_keyframes, "execute_keyframes")
+        server.register_function(self.get_transform, "get_transform")
+        server.register_function(self.set_transform, "set_transform")
+
+        # Create a new thread that runs the server's serve_forever method
+        # This allows the server to handle requests in the background
+        thread = threading.Thread(target=server.serve_forever)
+
+        # Start the thread, initiating the server's request handling loop
+        thread.start()
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        angle = self.perception.joint[joint_name]
+        return angle
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
-        # YOUR CODE HERE
+        self.target_joints[joint_name] = angle        
 
     def get_posture(self):
         '''return current posture of robot'''
-        # YOUR CODE HERE
+        posture = self.recognize_posture(self.perception)
+        return posture
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
+        :param keyframes: A tuple containing the names of the motors, their corresponding times, and key values.
         '''
-        # YOUR CODE HERE
+        # Unpack the keyframes tuple into names, times, and keys
+        (names, times, keys) = keyframes
+
+        # Calculate the duration of the longest keyframe sequence
+        # It finds the latest time point at which any motor movement ends
+        duration = max(map(self.last, times))
+
+        # Store the keyframes as an attribute of the object
+        self.keyframes = keyframes
+
+        # Block the execution until the last movement is completed
+        # This is done by pausing the thread for the duration of the longest keyframe sequence
+        time.sleep(duration)
+
+    def last(self, sequence):
+        '''Returns the last element of a sequence
+        Helper function
+        '''
+        return sequence[-1]
 
     def get_transform(self, name):
         '''get transform with given name
         '''
-        # YOUR CODE HERE
+        transform = self.transforms[name]
+        return transform
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
-        # YOUR CODE HERE
+        self.transforms[effector_name] = transform
 
 if __name__ == '__main__':
     agent = ServerAgent()
